134041cfa3f50a25b29009d6aae772cad480528a
matheusgondra <matheusgondra951@gmail.com>
2025-03-14 09:24:30 -0300

    componente header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -7,9 +7,9 @@ interface StoreLayoutProps {
 
 export default function StoreLayout({ children }: StoreLayoutProps) {
 	return (
-		<>
+		<div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
 			<Header />
 			{children}
-		</>
+		</div>
 	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="pt-BR">
-			<body className={inter.variable}>{children}</body>
+		<html lang="pt-BR" className={inter.variable}>
+			<body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
 		</html>
 	);
 }
